9063978480ef4b69d16dbd75b144131309a01138
yuksekyusuf <68251921+yuksekyusuf@users.noreply.github.com>
2024-11-29 15:35:54 -0500

    Initial Commit

---
+++ Custom Cache Image/Custom_Cache_ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_Cache_ImageApp.swift
+//  Custom Cache Image
+//
+//  Created by Ahmet Yusuf Yuksek on 29.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Custom_Cache_ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
